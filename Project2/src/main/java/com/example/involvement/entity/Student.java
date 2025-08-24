0917e90eacf6606a43b50b39ab01d0b7864327e5
Yashsavi Negi <negiyashsavi@gmail.com>
2025-08-24 13:29:02 -0500

    Student entity

---
+++ Project2/src/main/java/com/example/involvement/entity/Student.java
@@ -0,0 +1,46 @@
+package com.example.involvement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+    private String major;
+
+    public Student() {}
+    public Student(String name, String email, String major) {
+        this.name = name;
+        this.email = email;
+        this.major = major;
+    }
+
+    public int getId() { 
+        return id;
+    }
+    public String getName() { 
+        return name;
+    }
+    public void setName(String name) { 
+        this.name = name;
+    }
+    public String getEmail() { 
+        return email; 
+    }
+    public void setEmail(String email) {
+         this.email = email; 
+    }
+    public String getMajor() { 
+        return major; 
+    }
+    public void setMajor(String major) {
+         this.major = major; 
+    }
+}
